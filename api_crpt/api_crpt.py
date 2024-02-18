b6ee05830af74a4a971dfbdf0dce9bb83ce983c8
aruytehno <aruytehno@yandex.ru>
2024-02-18 14:21:07 +0300

    Update code

--- api_crpt/api_crpt.py
+++ api_crpt/api_crpt.py
@@ -6,25 +6,10 @@ import os
 
 '''
 Скрипт для проверки валидности кодов - находятся ли они в обороте или нет.
-'''
 
-class Lib:
-    def __init__(self):
-        pass
+Описание API ГИС МТ
+https://znak-it.ru/wp-content/uploads/2022/04/api-gis-mt.pdf
 
-    def _get(self, content, type):
-        return requests.get(f"https://mobile.api.crpt.ru/mobile/check?code={content}&codeType={type}").json()
-
-    def infoFromDataMatrix(self, xyematrix):
-        return self._get(xyematrix, "datamatrix")
-
-    def infoFromEAN13(self, ean13):
-        return self._get(ean13, "ean13")
-
-    def infoFromQr(self, qr):
-        return self._get(qr, "qr")
-
-'''
 Статус товара/КИ:
 EMITTED – Эмитирован. Выпущен;
 APPLIED – Эмитирован. Получен;
@@ -33,6 +18,22 @@ WRITTEN_OFF – КИ списан;
 RETIRED – Выбыл;
 DISAGGREGATION – Расформирован (только для упаковок)
 '''
+class Lib:
+    def __init__(self):
+        pass
+
+    def _get(self, content, type):
+        return requests.get(f"https://mobile.api.crpt.ru/mobile/check?code={content}&codeType={type}").json()
+
+    def infoFromDataMatrix(self, datamatrix):
+        return self._get(datamatrix, "datamatrix")
+
+    def infoFromEAN13(self, ean13):
+        return self._get(ean13, "ean13")
+
+    def infoFromQr(self, qr):
+        return self._get(qr, "qr")
+
 def getInfoFromDataMatrix(content, type):
     content = quote_plus(content)  # для кодирования символов при передаче AP
     link = f"https://mobile.api.crpt.ru/mobile/check?code={content}&codeType={type}"
--- fix_lines/fix_lines.py
+++ fix_lines/fix_lines.py
@@ -1,3 +1,37 @@
 '''
 Скрипт для замены двойных линий на одинарные в выгруженных файлах для удобства нарезки после печати.
 '''
+# https://www.pythonsos.com/libraries/drawing-lines-on-pdf-with-python/?expand_article=1
+import os
+import PyPDF2
+
+import PdfReader
+
+def draw_lines_on_pdf(input_pdf_path, output_pdf_path, line_coordinates):
+    with open(input_pdf_path, 'rb') as file:
+        pdf = PyPDF2.PdfFileReader(file)
+        page = pdf.getPage(0)
+        page_width = page.mediaBox.getWidth()
+        page_height = page.mediaBox.getHeight()
+
+        # content_stream = page['/Contents'].getObject()
+        # content_stream.append(PyPDF2.pdf.ContentStream('qn'))
+        #
+        # for coordinates in line_coordinates:
+        #     x1, y1, x2, y2 = coordinates
+        #     content_stream.append(PyPDF2.pdf.ContentStream(f'{x1} {y1} {x2} {y2} re Sn'))
+        #
+        # content_stream.append(PyPDF2.pdf.ContentStream('Qn'))
+        # page['/Contents'] = content_stream
+        #
+        # with open(output_pdf_path, 'wb') as output_file:
+        #     pdf.write(output_file)
+
+
+if __name__ == "__main__":
+    # Example usage
+    print(os.listdir())
+    line_coordinates = [(100, 100, 200, 200), (300, 300, 400, 400)]
+    draw_lines_on_pdf('input.pdf', 'output.pdf', line_coordinates)
+
+
