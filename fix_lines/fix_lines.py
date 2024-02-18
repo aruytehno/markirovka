8f914f9adf3247701761abca1731f7002dd1aa84
aruytehno <aruytehno@yandex.ru>
2024-02-18 20:29:44 +0300

    Update code

--- fix_lines/fix_lines.py
+++ fix_lines/fix_lines.py
@@ -1,37 +1,23 @@
 '''
 Скрипт для замены двойных линий на одинарные в выгруженных файлах для удобства нарезки после печати.
 '''
-# https://www.pythonsos.com/libraries/drawing-lines-on-pdf-with-python/?expand_article=1
-import os
-import PyPDF2
 
-import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
 
-def draw_lines_on_pdf(input_pdf_path, output_pdf_path, line_coordinates):
-    with open(input_pdf_path, 'rb') as file:
-        pdf = PyPDF2.PdfFileReader(file)
-        page = pdf.getPage(0)
-        page_width = page.mediaBox.getWidth()
-        page_height = page.mediaBox.getHeight()
+def put_watermark(input_pdf, output_pdf, watermark):
+    watermark_instance = PdfReader(watermark)
+    watermark_page = watermark_instance.pages[0]
+    pdf_reader = PdfReader(input_pdf)
+    pdf_writer = PdfWriter()
 
-        # content_stream = page['/Contents'].getObject()
-        # content_stream.append(PyPDF2.pdf.ContentStream('qn'))
-        #
-        # for coordinates in line_coordinates:
-        #     x1, y1, x2, y2 = coordinates
-        #     content_stream.append(PyPDF2.pdf.ContentStream(f'{x1} {y1} {x2} {y2} re Sn'))
-        #
-        # content_stream.append(PyPDF2.pdf.ContentStream('Qn'))
-        # page['/Contents'] = content_stream
-        #
-        # with open(output_pdf_path, 'wb') as output_file:
-        #     pdf.write(output_file)
+    for page in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page]
+        page.merge_page(watermark_page)
+        pdf_writer.add_page(page)
+
+    with open(output_pdf, 'wb') as out:
+        pdf_writer.write(out)
 
 
 if __name__ == "__main__":
-    # Example usage
-    print(os.listdir())
-    line_coordinates = [(100, 100, 200, 200), (300, 300, 400, 400)]
-    draw_lines_on_pdf('input.pdf', 'output.pdf', line_coordinates)
-
-
+    put_watermark('input.pdf', 'output.pdf', 'watermark.pdf')
