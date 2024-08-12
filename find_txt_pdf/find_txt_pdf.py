3bd39fa8d5e9e57c4e96a592f6296afbb72068dd
aruytehno <aruytehno@yandex.ru>
2024-08-12 17:18:45 +0300

    Update

--- find_txt_pdf/find_txt_pdf.py
+++ find_txt_pdf/find_txt_pdf.py
@@ -1,7 +1,8 @@
 '''
-Скрипт предназначен для поиска требуемых кодов в pdf документе с общим массивом кодов после выгрузки,
-сохраняя вырезанные в новый pdf файл.
+Скрипт предназначен для поиска требуемых кодов в PDF-документе с общим массивом кодов после выгрузки,
+сохраняя вырезанные в новый PDF-файл.
 '''
+
 import glob
 import logging
 import os
@@ -12,18 +13,23 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
-# count pages
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import resolve1
-# extract image
 from PyPDF2 import PdfWriter, PdfReader
-# folder
-
 from api_crpt.api_crpt import CodeChecker
 
 
 def extract_image(x, y, index_page, file_pdf_reader):
+    """
+    Извлекает обрезанную часть страницы PDF на основе координат и индекса страницы.
+
+    :param x: Координата X центра области для обрезки (float).
+    :param y: Координата Y центра области для обрезки (float).
+    :param index_page: Индекс страницы для обработки (int).
+    :param file_pdf_reader: Объект PdfReader для чтения страниц PDF.
+    :return: Обрезанную страницу в формате PdfReader.
+    """
     crop_page = file_pdf_reader.pages[index_page - 1]
     crop_page.mediabox.left = x - 38
     crop_page.mediabox.right = x + 62
@@ -33,14 +39,22 @@ def extract_image(x, y, index_page, file_pdf_reader):
 
 
 def find_coordinates(search_codes, list_input_files, target_folder):
+    """
+    Ищет указанные коды в списке PDF-документов и сохраняет вырезанные страницы с найденными кодами в новый PDF-файл.
+
+    :param search_codes: Список кодов для поиска (list).
+    :param list_input_files: Список путей к входным PDF-файлам (list).
+    :param target_folder: Папка для сохранения результата (str).
+    :return: None
+    """
     name_file = []
     lines_not_found = search_codes.copy()
     file_pdf_writer = PdfWriter()
+
     for file_pdf in list_input_files:
         fp = open(file_pdf, 'rb')
         file_pdf_reader = PdfReader(file_pdf)
 
-        # This will give you the count of pages
         parser = PDFParser(fp)
         document = PDFDocument(parser)
         count_pages = resolve1(document.catalog['Pages'])['Count']
@@ -50,6 +64,7 @@ def find_coordinates(search_codes, list_input_files, target_folder):
         device = PDFPageAggregator(rsrcmgr, laparams=laparams)
         interpreter = PDFPageInterpreter(rsrcmgr, device)
         pages = PDFPage.get_pages(fp)
+
         index_page = 0
         index_count = 0
 
@@ -92,6 +107,12 @@ def find_coordinates(search_codes, list_input_files, target_folder):
 
 
 def print_data_code(substring):
+    """
+    Получает информацию о коде и возвращает отформатированные данные.
+
+    :param substring: Код для проверки (str).
+    :return: Список с отформатированной информацией о коде и именем файла (list).
+    """
     i_out = ''
     code_checker = CodeChecker()
     data_code = code_checker.get_info(substring, "datamatrix")
@@ -101,18 +122,37 @@ def print_data_code(substring):
 
 
 def print_name_file(name_file, lines):
+    """
+    Форматирует имя файла для сохранения, заменяя нежелательные символы.
+
+    :param name_file: Список имен файлов (list).
+    :param lines: Список не найденных кодов (list).
+    :return: Отформатированное имя файла (str).
+    """
     replace_values = {"/": "%", "[": "", "]": "", "\'": "", "\"": ""}
     name = str(list(set(name_file))) + ' (' + str(len(lines)) + ' pcs).pdf'
     return multiple_replace(name, replace_values)
 
 
 def multiple_replace(target_str, replace_values):
+    """
+    Заменяет все вхождения подстрок в строке на заданные значения.
+
+    :param target_str: Целевая строка (str).
+    :param replace_values: Словарь замен, где ключи - это подстроки для замены, а значения - это заменяющие строки (dict).
+    :return: Измененную строку (str).
+    """
     for i, j in replace_values.items():
         target_str = target_str.replace(i, j)
     return target_str
 
 
 if __name__ == "__main__":
+    """
+    Основной блок кода выполняется при запуске скрипта как основной программы.
+    Создаются необходимые папки, считываются пути к входным PDF-файлам и коды для поиска,
+    и вызывается функция find_coordinates для поиска кодов и сохранения результатов.
+    """
     list_folders = ['input', 'out']
     for folder in list_folders:
         if not os.path.exists(folder):
