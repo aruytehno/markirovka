08c1fa13886268fffafa8b9f37d2b10d17c6c1d4
aruytehno <aruytehno@yandex.ru>
2024-08-12 16:45:44 +0300

    Update

--- fix_lines/fix_lines.py
+++ fix_lines/fix_lines.py
@@ -1,35 +1,49 @@
-'''
-Скрипт для замены двойных линий на одинарные в выгруженных файлах для удобства нарезки после печати.
-'''
 import glob
-
 from PyPDF2 import PdfReader, PdfWriter
 
 
-def put_watermark(input_pdf, output_pdf, watermark):
-    watermark_instance = PdfReader(watermark)
-    watermark_page = watermark_instance.pages[0]
-    pdf_reader = PdfReader(input_pdf)
+def add_watermark_to_pdf(input_pdf_path, output_pdf_path, watermark_pdf_path):
+    """
+    Добавляет водяной знак ко всем страницам PDF-файла.
+
+    :param input_pdf_path: Путь к исходному PDF-файлу, который нужно обработать.
+    :param output_pdf_path: Путь к выходному PDF-файлу с добавленным водяным знаком.
+    :param watermark_pdf_path: Путь к PDF-файлу с водяным знаком.
+    :return: Кортеж с количеством обработанных страниц и общим числом страниц.
+    """
+    # Создаем объект PdfReader для водяного знака и первой страницы водяного знака
+    watermark_reader = PdfReader(watermark_pdf_path)
+    watermark_page = watermark_reader.pages[0]
+
+    # Создаем объект PdfReader для входного PDF и PdfWriter для записи выходного PDF
+    pdf_reader = PdfReader(input_pdf_path)
     pdf_writer = PdfWriter()
 
-    i = 0
-    len_pdf = len(pdf_reader.pages)
+    total_pages = len(pdf_reader.pages)
 
-    for page in range(len_pdf):
-        page = pdf_reader.pages[page]
+    # Проходим по каждой странице входного PDF и добавляем водяной знак
+    for page_num in range(total_pages):
+        page = pdf_reader.pages[page_num]
         page.merge_page(watermark_page)
         pdf_writer.add_page(page)
-        i = i + 1
 
-    with open(output_pdf, 'wb') as out:
-        pdf_writer.write(out)
-    return [i, len_pdf]
+    # Записываем результат в выходной файл
+    with open(output_pdf_path, 'wb') as out_file:
+        pdf_writer.write(out_file)
+
+    return total_pages, total_pages
 
 
 if __name__ == "__main__":
-    list_files = glob.glob("*.pdf")
-    list_files.remove('watermark.pdf')
-    for file in list_files:
-        print('\nОбрабатывается: ' + file)
-        len_pdf = put_watermark(file, file.replace('.pdf', '_fix_lines.pdf'), 'watermark.pdf')
-        print('Обрабатано: ' + str(len_pdf[0]) + ' из ' + str(len_pdf[1]) + ' страниц.\n')
+    # Получаем список всех PDF-файлов в текущем каталоге, кроме водяного знака
+    pdf_files = [f for f in glob.glob("*.pdf") if f != 'watermark.pdf']
+
+    # Обрабатываем каждый файл
+    for pdf_file in pdf_files:
+        print(f'\nОбрабатывается: {pdf_file}')
+        total_pages, processed_pages = add_watermark_to_pdf(
+            pdf_file,
+            pdf_file.replace('.pdf', '_fix_lines.pdf'),
+            'watermark.pdf'
+        )
+        print(f'Обработано: {processed_pages} из {total_pages} страниц.\n')
